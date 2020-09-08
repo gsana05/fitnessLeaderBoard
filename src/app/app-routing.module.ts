b13ac51d8378269c6b8a76c1ff3cc5c6deadc7e7
GarethSanashee <gareth.sanashee@appsinthesky.com>
2020-09-08 15:17:14 +0100

    added sign up component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signup', component:SignUpComponent},
+  { path: '**', redirectTo: '/signup' }, //goes to this page by default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
